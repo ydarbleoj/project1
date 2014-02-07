89c4a40f948d3bb761e189936b07431fc2ac3e51
ydarbleoj <brady.joel@gmail.com>
2014-02-07 08:48:37 -0800

    removing loose files

--- app/models/relationship.rb
+++
@@ -1,8 +0,0 @@
-class Relationship < ActiveRecord::Base
-  belongs_to :follower, class_name: "Player"
-  belongs_to :followed, class_name: "Player"
-  validates :follower_id, presence: true 
-  validates :followed_id, presence: true 
-
-  
-end
